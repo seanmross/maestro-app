0c38ca6b5b21469ba95b6b4bedfa5f1f58440a7e
Sean Ross <seanross917@gmail.com>
2019-12-13 17:16:51 -0500

    added basic playback controls, styled wave with new colors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './home/home.component';
 
-import { MaterialModule } from "./material.module";
+import { HomeComponent } from './home/home.component';
+import { MaterialModule } from './material/material.module';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
+    LayoutModule,
     MaterialModule,
     BrowserModule,
     AppRoutingModule,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Breakpoints, BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 import WaveSurfer from 'wavesurfer.js';
 import TimelinePlugin from 'wavesurfer.js/dist/plugin/wavesurfer.timeline.min.js';
-// import MinimapPlugin from 'wavesurfer.js/dist/plugin/wavesurfer.minimap.min.js';
+import CursorPlugin from 'wavesurfer.js/dist/plugin/wavesurfer.cursor.min.js';
 
 @Component({
   selector: 'app-home',
@@ -13,27 +14,48 @@ export class HomeComponent implements OnInit {
 
   waveReady = false;
 
-  constructor() { }
+  constructor(
+    private cdr: ChangeDetectorRef,
+    public bpObserver: BreakpointObserver
+  ) { }
 
   ngOnInit() {
     this.wave = WaveSurfer.create({
       container: '#waveform',
-      waveColor: 'voilet',
-      progressColor: 'purple',
-      plugins: [
-        TimelinePlugin.create({
-          container: '#wave-timeline'
-        })
-      ]
+      progressColor: '#57a260',
+      waveColor: '#8e8e8e',
+      cursorColor: '#ec407a',
+      cursorWidth: 3,
+      barWidth: 2,
+      barGap: 2,
+      responsive: true
+      // plugins: [
+      //   TimelinePlugin.create({
+      //     container: '#wave-timeline'
+      //   }),
+      //   CursorPlugin.create({
+      //     showTime: true,
+      //     opacity: 1,
+      //   })
+      // ],
     });
     
     this.wave.on('ready', () => {
       this.waveReady = true;
-      console.log('wave ready')
-      console.log(this.waveReady)
+      this.cdr.detectChanges();
     });
     
     this.wave.load('assets/mp3/akshay_nanavati.mp3');
+
+    this.bpObserver
+      .observe([Breakpoints.Small, Breakpoints.HandsetPortrait])
+      .subscribe((state: BreakpointState) => {
+        if (state.matches) {
+          console.log('Matches small viewport or handset in portrait mode');
+        } else {
+          console.log('Viewport is getting smaller!');
+        }
+      });
   }
 
   onPlay() {
